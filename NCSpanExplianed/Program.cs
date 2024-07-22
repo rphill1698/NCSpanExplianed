5ee470bbbb38862001df63adab011b6b052fd92f
Russ <rphill1698@yahoo.com>
2024-07-22 08:26:09 +0100

    Add project files.

---
+++ NCSpanExplianed/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace NCSpanExplianed
+{
+    internal class Program
+    {
+        private static readonly string _dateAsText = "08 07 2021";
+        static void Main(string[] args)
+        {
+            var date = DateWithStringAndSubsctring();
+            Console.WriteLine(date);
+        }
+
+        private static (int day, int month, int year) DateWithStringAndSubsctring()
+        {
+            var dayAsText = _dateAsText.Substring(0, 2);
+            var monthAsText = _dateAsText.Substring(3, 2);
+            var yearAsText = _dateAsText.Substring(6);
+
+            var day = int.Parse(dayAsText);
+            var month = int.Parse(monthAsText);
+            var year = int.Parse(yearAsText);
+
+            return (day, month, year);
+
+            //return (8, 7, 2021);
+        }
+    }
+}
